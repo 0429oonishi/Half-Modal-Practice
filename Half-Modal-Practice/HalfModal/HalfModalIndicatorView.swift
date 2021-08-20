31c28c050a2259fecbb5cc82ef8cdf89f95b8f74
0429oonishi <oonishireon110@gmail.com>
2021-08-21 07:27:38 +0900

    HalfModalIndicatorView実装

--- Half-Modal-Practice/HalfModal/HalfModalIndicatorView.swift
+++ Half-Modal-Practice/HalfModal/HalfModalIndicatorView.swift
@@ -5,4 +5,24 @@
 //  Created by 大西玲音 on 2021/08/21.
 //
 
-import Foundation
+import UIKit
+
+final class HalfModalIndicatorView: UIView {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupSelf()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSelf()
+    }
+    
+    private func setupSelf() {
+        layer.masksToBounds = true
+        layer.cornerRadius = 5
+        backgroundColor = .lightGray
+    }
+    
+}
