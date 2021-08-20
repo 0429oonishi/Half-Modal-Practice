e91cfe0ce69ffec3d4a375451b6e028b71d56055
0429oonishi <oonishireon110@gmail.com>
2021-08-21 07:27:52 +0900

    HalfModalOverlayView実装

--- Half-Modal-Practice/HalfModal/HalfModalOverlayView.swift
+++ Half-Modal-Practice/HalfModal/HalfModalOverlayView.swift
@@ -5,4 +5,29 @@
 //  Created by 大西玲音 on 2021/08/21.
 //
 
-import Foundation
+import UIKit
+
+final class HalfModalOverlayView: UIView {
+    
+    var isActive: Bool = false {
+        didSet {
+            alpha = isActive ? 0.5 : 0
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupSelf()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSelf()
+    }
+    
+    private func setupSelf() {
+        backgroundColor = .black
+        alpha = 0.5
+    }
+    
+}
