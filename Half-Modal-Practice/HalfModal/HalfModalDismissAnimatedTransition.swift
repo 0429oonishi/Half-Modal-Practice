a7232ca339803438d9cd8146b199f9f546700505
0429oonishi <oonishireon110@gmail.com>
2021-08-21 07:27:10 +0900

    HalfModalDismissAnimatedTransition作成

--- Half-Modal-Practice/HalfModal/HalfModalDismissAnimatedTransition.swift
+++ Half-Modal-Practice/HalfModal/HalfModalDismissAnimatedTransition.swift
@@ -5,4 +5,27 @@
 //  Created by 大西玲音 on 2021/08/21.
 //
 
-import Foundation
+import UIKit
+
+final class HalfModalDismissAnimatedTransition: NSObject {
+    
+}
+
+extension HalfModalDismissAnimatedTransition: UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.4
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        UIView.animate(withDuration: transitionDuration(using: transitionContext),
+                       delay: 0,
+                       options: .curveEaseInOut) {
+            guard let fromView = transitionContext.view(forKey: .from) else { return }
+            fromView.center.y = UIScreen.main.bounds.size.height + fromView.bounds.height / 2
+        } completion: { _ in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+    }
+    
+}
